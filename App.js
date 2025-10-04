fd6b55dda304037b7946d64e6d6f64252e138056
Yogesh-yo01 <syokesh081@gmail.com>
2025-10-04 19:19:49 +0530

    App Icon Updated

--- App.js
+++ App.js
@@ -1,7 +1,39 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import SplashScreen from './src/SplashScreen/SplashScreen';
+
+const Stack = createStackNavigator();
 const App = () => {
+  const [splashVisible, setSplashVisible] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isFirstLaunch, setIsFirstLaunch] = useState(false);
+  const [profileSetup, setProfilesetup] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSplashVisible(false);
+    }, 2050);
+    AsyncStorage.getItem('alreadyLaunched').then(value => {
+      if (value === null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true); //For First Time Show Onboarding Screen
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+    // getFCMToken();
+    // checkLoginStatus();
+  }, []);
+  if (splashVisible) {
+    return (
+      <SplashScreen/>
+    );
+  }
   return (
     <View>
       <Text>App.js</Text>
---
+++ src/Theme/Theme.js
@@ -0,0 +1,73 @@
+export const Fonts = {
+    thin: 'Metropolis-Thin',
+    light: 'Metropolis-Light',
+    extralight: 'Metropolis-ExtraLight',
+    regular: 'Metropolis-Regular',
+    medium: 'Metropolis-Medium',
+    semiBold: 'Metropolis-SemiBold',
+    bold: 'Metropolis-Bold',
+    extrabold: 'Metropolis-ExtraBold',
+
+    regularAS: 'AlbertSans-Regular',
+    boldAS: 'AlbertSans-Bold',
+    lightAS: 'AlbertSans-Light',
+    mediumAS: 'AlbertSans-Medium',
+    semiBoldAS: 'AlbertSans-SemiBold',
+};
+export const Colors = {
+    primary: '#1B51BB',
+    primary_text_1B2436: '#1B2436',
+
+    secondary_668AD1: '#668AD1',
+    
+    
+    blue_668AD1: '#668AD1',
+    blue_8493B2: '#8493B2',
+    blue_D6E7FF: '#D6E7FF',
+    blue_E0E9FB: '#E0E9FB',
+    blue_bg_F2F8FF: '#F2F8FF',
+    placeholder: '#B2C2D2',
+
+    red_FF0202: '#FF0202',
+    red_bg_FFDBDB: '#FFDBDB',
+
+    green_45B129: '#45B129',
+    green_bg_E7F8E2: '#E7F8E2',
+
+
+    blue_090040: '#090040',
+    blue_Text_090040: '#090040',
+
+    bg_white: '#F1F6EE',
+    bg_EEF2F6: '#EEF2F6',
+    white_fffff: '#FFFFFF',
+    white_EEF2F6: '#EEF2F6',
+    white_F3F6FB: '#F3F6FB',
+    white_F2F8FF: '#F2F8FF',
+
+    sandal_light_FFEDD1: '#FFEDD1',
+    sandal_FFAA22: '#FFAA22',
+
+    text_color: '#030303',
+    Black_000000: "#000000",
+    red: '#E50000',
+    gray: '#5D6167', //secondary text color
+    placeHolder_gray: '#B4B4B4',
+    gray9D9D9D: '#9D9D9D',
+    grey_656565: '#656565',
+};
+
+export const Size = {
+    sm_8: 8,
+    sm_10: 10,
+    sm_12: 12,
+    sm_14: 14,
+    md_16: 16,
+    md_18: 18,
+    md_20: 20,
+    md_22: 22,
+    lg_24: 24,
+    lg_26: 26,
+    lg_28: 28,
+    lg_30: 30,
+};
